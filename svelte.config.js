a2991ac93f869b8e286405221abad417d473c66d
renzp94 <1074720760@qq.com>
2022-03-31 14:53:46 +0800

    chore: deploy

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
 		target: '#svelte',
 		adapter: adapter({
 			// default options are shown
-			pages: 'build',
-			assets: 'build',
+			pages: 'dist',
+			assets: 'dist',
 			fallback: null
 		})
 	}
