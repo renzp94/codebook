9ca7d7072d3345e182c6a9b0c81e815423833b06
renzp94 <1074720760@qq.com>
2023-06-06 15:52:37 +0800

    chore: 调整文章列表排序

--- src/routes/book/[book]/+page.server.ts
+++ src/routes/book/[book]/+page.server.ts
@@ -2,7 +2,7 @@ import { loadMarkdownDir } from '../../../markdown'
 import type { PageServerLoad } from './$types'
 
 export const load:PageServerLoad = (({ params: { book } }) => {
-	const articles = loadMarkdownDir(book)
+	const articles = loadMarkdownDir(book).reverse()
 
 	return { articles, category: book }
 })
